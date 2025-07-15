9d652ecbe36a47525801a1b50f49748905b7cfe7
vida26365 <vm36265@student.uni-lj.si>
2025-07-15 12:20:42 +0200

    Dodajanje oseb (ampak se dodajo v ključne besede za zdej)

--- src/components/documents/input.rs
+++ src/components/documents/input.rs
@@ -52,7 +52,7 @@ async fn submit(mut document: DocumentActiveModel, event: Event<FormData>) {
         document.date = Set(Some(date));
     }
 
-    // TODO: Handle persons
+    //TODO: Persons
     // TODO: Handle organizations
     // TODO: Handle locations
 
@@ -218,12 +218,102 @@ fn InputDate(document: DocumentParam) -> Element {
 
 #[component]
 fn InputPersons(persons: PersonsParam) -> Element {
+    let mut persons = use_signal(move || persons.read().clone().into_iter().map(|(person, _)| person.name).collect::<Vec<_>>());
+    let mut additional = use_signal(String::new);
+
     rsx! {
         label {
             class: "flex pb-2 font-semibold",
             "Osebe"
         }
-        span { "TODO" }
+
+        for oseba in persons.read().iter().cloned() {
+            div {
+                class: "input w-full mb-2",
+                input {
+                    aria_autocomplete: "none",
+                    autocapitalize: "false",
+                    autocomplete: "false",
+                    spellcheck: "false",
+                    name: "keywords",
+                    value: "{oseba}",
+                    oninput: {
+                        let keyword = oseba.clone();
+                        move |event: Event<FormData>| {
+                            let mut persons = persons.write();
+                            match persons.iter().position(|existing| existing == &keyword) {
+                                Some(pos) => persons[pos] = event.value(),
+                                None => persons.push(event.value()),
+                            }
+                        }
+                    },
+                    onkeypress: move |event| {
+                        if event.key() == Enter {
+                            event.prevent_default();
+                        }
+                    }
+                }
+                button {
+                    class: "cursor-pointer text-base-content/50 hover:text-base-content",
+                    onclick: {
+                        let keyword = oseba.clone();
+                        move |event: Event<MouseData>| {
+                            event.prevent_default();
+                            persons.write().retain(|existing| existing != &keyword);
+                        }
+                    },
+                    svg {
+                        class: "size-4 shrink-0",
+                        fill: "none",
+                        stroke: "currentColor",
+                        stroke_linecap: "round",
+                        stroke_linejoin: "round",
+                        stroke_width: "2",
+                        view_box: "0 0 24 24",
+                        { Shape::Trash.path() }
+                    }
+                }
+            }
+        }
+
+        div {
+            class: "input w-full",
+            input {
+                aria_autocomplete: "none",
+                autocapitalize: "false",
+                autocomplete: "false",
+                spellcheck: "false",
+                name: "keywords",
+                value: "{additional}",
+                oninput: move |event| {
+                    additional.set(event.value());
+                },
+                onkeypress: move |event| {
+                    if event.key() == Enter {
+                        event.prevent_default();
+                        persons.write().push(additional.read().clone());
+                        additional.set(String::new());
+                    }
+                }
+            }
+            button {
+                class: "cursor-pointer text-base-content/50 hover:text-base-content",
+                onclick: move |event| {
+                    event.prevent_default();
+                    additional.set(String::new());
+                },
+                svg {
+                    class: "size-4 shrink-0",
+                    fill: "none",
+                    stroke: "currentColor",
+                    stroke_linecap: "round",
+                    stroke_linejoin: "round",
+                    stroke_width: "2",
+                    view_box: "0 0 24 24",
+                    { Shape::Trash.path() }
+                }
+            }
+        }
     }
 }
 
